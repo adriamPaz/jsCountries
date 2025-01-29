5563e95ac3bf3ba8a178f1a5bbc1c6cfae37ad1b
dam1 - Paz Sánchez <A24AdriamPS@sanclemente.local>
2025-01-29 14:19:46 +0100

    adelante y atras hecho

--- js.js
+++ js.js
@@ -1,3 +1,22 @@
-function changePage(){
+function changePage1(currentId, nextId){
+    let currentCard = document.getElementById(currentId);
+    let nextCard = document.getElementById(nextId);  
+
+    currentCard.classList.add("d-none");
+
+    nextCard.classList.remove("d-none");
+    
+}
+
+function changeBackwards(currentId,pastId){
+    let currentCard = document.getElementById(currentId);
+    let pastCard = document.getElementById(pastId);
+
+    currentCard.classList.add("d-none");
+    pastCard.classList.remove("d-none");
+
+}
+
+function trackScore(){
     
 }
